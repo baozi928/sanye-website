4378c8d0a2c80ddc6cc289d31a237782d7f92f75
Lily <2542101862@qq.com>
2022-09-04 16:51:48 +0800

    deploy

---
+++ precache-manifest.57dad825918f9f8a717256ccc1d2339c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d2c3ac5db5e1f2f77d",
+    "url": "/sanye-website/css/app.e168cf9f.css"
+  },
+  {
+    "revision": "42f948f3cac89ccf1eeb1371d08fc8ee",
+    "url": "/sanye-website/index.html"
+  },
+  {
+    "revision": "16d2c3ac5db5e1f2f77d",
+    "url": "/sanye-website/js/app.f4c255ab.js"
+  },
+  {
+    "revision": "3434dd8ea0405f106ada",
+    "url": "/sanye-website/js/chunk-vendors.41ef144e.js"
+  },
+  {
+    "revision": "c565786e74483ef27f66ee4f47047478",
+    "url": "/sanye-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sanye-website/robots.txt"
+  }
+]);
